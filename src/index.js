981c49c526b3e198cd3ba2294e6be2e859130b95
junkor-1011 <tomoyukizxy@gmail.com>
2021-01-11 22:03:45 +0900

    delete comment

--- src/index.js
+++ src/index.js
@@ -18,21 +18,6 @@ const Square = (props) => {
 
 const Board = (props) => {
 
-  // const [squares, setSquares] = useState(
-  //   Array(9).fill(null)
-  // );
-  // const [xIsNext, setXIsNext] = useState(true);
-
-  // const handleClick = (i) => {
-  //   const squaresDummy = squares.slice();
-  //   if (calculateWinner(squaresDummy) || squaresDummy[i]) {
-  //     return
-  //   }
-  //   squaresDummy[i] = xIsNext ? 'X': 'O';
-  //   setSquares(squaresDummy);
-  //   setXIsNext(!xIsNext);
-  // }
-
   const renderSquare = (i) => {
     return (
       <Square
@@ -43,9 +28,6 @@ const Board = (props) => {
   }
 
   const winner = calculateWinner(props.squares);
-  // const status = winner ?
-  //   `Winner: ${winner}`:
-  //   `Next player: ${xIsNext ? 'X' : 'O'}`;
 
   return (
     <div>
@@ -68,37 +50,6 @@ const Board = (props) => {
     </div>
   )
 }
-
-// class Board extends React.Component {
-//   renderSquare(i) {
-//     return <Square />;
-//   }
-// 
-//   render() {
-//     const status = 'Next player: X';
-// 
-//     return (
-//       <div>
-//         <div className="status">{status}</div>
-//         <div className="board-row">
-//           {this.renderSquare(0)}
-//           {this.renderSquare(1)}
-//           {this.renderSquare(2)}
-//         </div>
-//         <div className="board-row">
-//           {this.renderSquare(3)}
-//           {this.renderSquare(4)}
-//           {this.renderSquare(5)}
-//         </div>
-//         <div className="board-row">
-//           {this.renderSquare(6)}
-//           {this.renderSquare(7)}
-//           {this.renderSquare(8)}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 const Game = () => {
 
@@ -148,22 +99,6 @@ const Game = () => {
   )
 }
 
-// class Game extends React.Component {
-//   render() {
-//     return (
-//       <div className="game">
-//         <div className="game-board">
-//           <Board />
-//         </div>
-//         <div className="game-info">
-//           <div>{/* status */}</div>
-//           <ol>{/* TODO */}</ol>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
 const calculateWinner = squares => {
   const lines = [
     [0, 1, 2],
@@ -175,12 +110,6 @@ const calculateWinner = squares => {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  // lines.forEach(line => {
-  //   const [a, b, c] = line;
-  //   if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-  //     return squares[a];
-  //   }
-  // });
   for (let i = 0; i< lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
